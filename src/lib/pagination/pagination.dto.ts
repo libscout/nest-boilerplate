2647638bcc92ad419210916873c99a32039a1529
libscout <libscout@outlook.com>
2026-05-15 14:13:33 +0400

    init

---
+++ src/lib/pagination/pagination.dto.ts
@@ -0,0 +1,17 @@
+import { Type } from 'class-transformer';
+import { IsInt, IsOptional, Min, Max } from 'class-validator';
+
+export class PaginationDto {
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  page?: number;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @Max(100)
+  limit?: number;
+}
